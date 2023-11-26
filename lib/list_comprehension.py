6c6d5023a830cbb94d769fe9519302a819479bf7
aminafarah <amishkamohamed563@gmail.com>
2023-11-26 16:02:28 +0300

    list

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    evens =[num for num in num_list if num % 2 == 0]
+    return evens
     pass
 
 def make_exclamation(sentence_list):
+    strings = [sentence +'!' for sentence in sentence_list]
+    return strings
+    sentence =["wooh" "i love python"]
+    print (make_exclamation(sentence))
     pass
